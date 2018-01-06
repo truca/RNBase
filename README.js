7282133fcb386fc3c05e123bb7e68725037a23f5
Ignacio Ureta <ignacio.ureta@live.com>
2018-01-06 14:20:07 -0300

    README updated with instructions for image uploading

---
+++ README.js
@@ -0,0 +1,16 @@
+use with Expo Develolment Enviroment (XDE)
+
+
+##Image Uploading
+
+For image upload, you will need a image handling server. This project is using 
+
+[clanapp-image-upload](https://gitlab.com/ignacio.ureta/clanapp-image-upload)
+
+###Development
+
+For development, it uses ngrok to build a tunnel (the link changes, so you have to upload the link in profile)
+
+###Production
+
+In production, the link wont change, so you dont need to keep changing the link
