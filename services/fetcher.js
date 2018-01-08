7d2eb9344dca3a4a8608d9a6f4c3157410ea7148
Ignacio Ureta <ignacio.ureta@live.com>
2018-01-07 23:02:10 -0300

    fetch first version

---
+++ services/fetcher.js
@@ -0,0 +1,42 @@
+let instance;
+
+export default class Fetcher {
+  route
+  dispatch
+  constructor(route, dispatch){
+    if(!instance){
+      instance = this;
+    }
+    this.route = route
+    this.dispatch = dispatch
+  }
+  get(path, headers, actionThunk){
+    this.operation(path, {
+      method: 'GET',
+      headers,
+    }, actionThunk)
+  }
+  delete(path, headers, actionThunk){
+    this.operation(path, {
+      method: 'DELETE',
+      headers,
+    }, actionThunk)
+  }
+  post(path, headers, actionThunk, data){
+    this.operation(path, {
+      method: 'POST',
+      headers,
+      body: JSON.stringify(data),
+    }, actionThunk)
+  }
+  put(path, headers, actionThunk, data){
+    this.operation(path, {
+      method: 'PUT',
+      headers,
+      body: JSON.stringify(data),
+    }, actionThunk)
+  }
+  operation(path, configuration, actionThunk){
+    return fetch(`${this.route}${path}`, config).then(res => this.dispatch( actionThunk(res) ))
+  }
+}
